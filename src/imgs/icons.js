143d82226f3f67fc53da24aa80e12014f633527e
barada84 <barada84@29-1>
2026-04-20 10:49:27 +0300

    icons

--- src/imgs/icons.js
+++ src/imgs/icons.js
@@ -17,3 +17,46 @@ export { default as iconLightning } from './⚡.png';
 export { default as iconShield } from './🛡️.png';
 export { default as iconChat } from './💬.png';
 export { default as iconStar } from './⭐.png';
+export { default as iconDarts } from './🎯.png';
+export { default as iconStudent } from './🎓.png';
+export { default as iconHome } from './🏠.png';
+export { default as iconTree } from './🌳.png';
+export { default as iconTriangle } from './📐.png';
+export { default as iconVessilya } from './👰.png';
+export { default as iconHeadphones } from './🎧.png';
+export { default as iconLupa } from './🔍.png';
+export { default as iconOffice } from './🏢.png';
+export { default as iconTassel } from './🖌️.png';
+export { default as iconCloud } from './☁️.png';
+export { default as iconGamePad } from './🎮.png';
+export { default as iconGlobus } from './🌐.png';
+export { default as iconBulb } from './💡.png';
+export { default as iconNotes } from './🎶.png';
+export { default as iconBook } from './📖.png';
+export { default as iconShine } from './✨.png';
+export { default as iconPlenka } from './🎞️.png';
+export { default as iconCircus } from './🎪.png';
+export { default as iconBlockChain } from './🔗.png';
+export { default as iconAI } from './🤖.png';
+export { default as iconEmail } from './📧.png';
+export { default as iconSiren } from './📢.png';
+export { default as iconMicrophone } from './🎤.png';
+export { default as iconNewspaper } from './📰.png';
+export { default as iconPencil } from './✏️.png';
+export { default as iconCamera } from './🎥.png';
+export { default as iconComedyMask } from './🎭.png';
+export { default as iconComedyMask } from './🎭.png';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
